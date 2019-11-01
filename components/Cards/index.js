0bd9696ff20ef45adb55db2761b168f64ac221ba
BryanKAdams <17adamsbryk@washk12.org>
2019-11-01 11:51:16 -0600

    finsihed mvp played around with carousel

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,61 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+function ArticleCards(item) {
+    const newCard = document.createElement("div"),
+        headline = document.createElement("div"),
+        author = document.createElement("div"),
+        imageContainer = document.createElement("div"),
+        newImage = document.createElement("img"),
+        authorsName = document.createElement("span");
+
+    newCard.appendChild(headline);
+    newCard.appendChild(author);
+    author.appendChild(imageContainer);
+    imageContainer.appendChild(newImage);
+    author.appendChild(authorsName);
+
+    //classes
+    newCard.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imageContainer.classList.add("img-container");
+    newImage.src = item.authorPhoto;
+    headline.textContent = item.headline;
+    authorsName.textContent = 'by ' + item.authorName;
+
+
+    //content 
+
+
+
+    return newCard;
+}
+const cardEntryPoint = document.querySelector(".cards-container");
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        Object.values(response.data.articles).forEach(item => {
+            item.forEach(articles => {
+                cardEntryPoint.append(ArticleCards(articles))
+            })
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+
+//The Object.values() method returns an array of a given object's own enumerable property values, 
+//in the same order as that provided by a for...in loop (the difference being that a for-in loop 
+// enumerates properties in the prototype chain as well).
+
+//Thank goodness for MDNs and Stackoverflow
+
+//
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+
+//https://stackoverflow.com/questions/14929819/why-is-there-no-foreach-method-on-object-in-ecmascript-5
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,51 @@
     <div class="right-button"> > </div>
   </div>
 */
+let carouselContainer = document.querySelector(".carousel-container")
+function carousel() {
+  const carousel = document.createElement("div"),
+  leftbutton = document.createElement("div"),
+  img1 = document.createElement("img"),
+  img2 = document.createElement("img"),
+  img3 = document.createElement("img"),
+  img4 = document.createElement("img"),
+  rightbutton = document.createElement("div");
+
+  carousel.appendChild(leftbutton);
+  carousel.appendChild(img1);
+  carousel.appendChild(img2);
+  carousel.appendChild(img3);
+  carousel.appendChild(img4);
+  carousel.appendChild(rightbutton);
+
+  //classes
+carousel.classList.add("carousel");
+leftbutton.textContent = "<"
+rightbutton.textContent = ">"
+leftbutton.classList.add("left-button");
+rightbutton.classList.add("right-button");
+img1.classList.add("hide-btn");
+
+  //content
+  
+  img1.src = "./assets/carousel/mountains.jpeg"
+  img2.src = "./assets/carousel/computer.jpeg"
+  img3.src = "./assets/carousel/trees.jpeg"
+  img4.src = "./assets/carousel/turntable.jpeg"
+
+ 
+  leftbutton.addEventListener("click", () => {
+    img1.classList.toggle('toggle-on');
+    if(img1.classList.contains('toggle-on')){
+    img1.classList.toggle('toggle-on');
+    img2.classList.toggle('toggle-on');
+    }
+    
+  });
+  
+  return carousel;
+  
+}
+
+carouselContainer.append(carousel());
+
